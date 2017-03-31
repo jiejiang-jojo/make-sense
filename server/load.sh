f352fe8d7c8d0eaff0d7a49150bd19b149b3f98c
Jie Jiang <jie.jiang@surrey.ac.uk>
2017-03-31 14:43:40 +0100

    Fix message printing in saving and loading scripts

--- server/load.sh
+++ server/load.sh
@@ -2,6 +2,6 @@
 
 IMAGE=${1:-server-hs}
 SRC=${2:-server-hs.img.gz}
-echo 'Going to load $SRC as $IMAGE in 3 seconds...'
+echo "Going to load $SRC as $IMAGE in 3 seconds..."
 sleep 3
 zcat "$SRC" | docker load "$IMAGE"
--- server/save.sh
+++ server/save.sh
@@ -2,6 +2,6 @@
 
 IMAGE=${1:-server-hs}
 DEST=${2:-server-hs.img.gz}
-echo 'Going to save $IMAGE to $DEST in 3 seconds...'
+echo "Going to save $IMAGE to $DEST in 3 seconds..."
 sleep 3
 docker save "$IMAGE" | gzip > "$DEST"
