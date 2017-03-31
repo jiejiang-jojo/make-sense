b9d244aa87ab06550d9b72119d9265ce2c9b86a7
Jie Jiang <jie.jiang@surrey.ac.uk>
2017-03-31 14:47:24 +0100

    Fix loading script

--- server/load.sh
+++ server/load.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 SRC=${1:-server-hs.img.gz}
-IMAGE=${2:-server-hs}
-echo "Going to load $SRC as $IMAGE in 3 seconds..."
+echo "Going to load $SRC in 3 seconds..."
 sleep 3
-zcat "$SRC" | docker load "$IMAGE"
+zcat "$SRC" | docker load
