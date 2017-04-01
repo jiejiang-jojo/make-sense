e8a02de45d6faca118581004b3d719c8b2c7f011
Jie Jiang <jie.jiang@surrey.ac.uk>
2017-04-01 16:33:02 +0100

    Customize Kibana image
    - Add HomeSense logo
    - Add an option of 1s refresh interval (the option of 45s is removed)

--- server/build.sh
+++ server/build.sh
@@ -1,1 +1,14 @@
-docker build -t server-hs .
+#!/bin/bash
+
+IMAGE=${1:-server-hs}
+
+case $IMAGE in
+  server-hs)
+    echo "Building server-hs ..."
+    docker build -f Dockerfile.server-hs -t server-hs .
+    ;;
+  kibana-hs)
+    echo "Building kibana-hs ..."
+    docker build -f Dockerfile.kibana-hs -t kibana-hs .
+    ;;
+esac
