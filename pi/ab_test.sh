89978f7526e0ed4ac17ef08265931a87232f1146
Jie Jiang <jie.jiang@surrey.ac.uk>
2017-04-02 11:19:11 +0100

    Increase ab_test pressure

--- pi/ab_test.sh
+++ pi/ab_test.sh
@@ -2,8 +2,8 @@
 
 
 URL=${1:-http://localhost/energy-record}
-REQUESTS=${2:-2000}
-THREADS=${3:-10}
+REQUESTS=${2:-10000}
+THREADS=${3:-200}
 PAYLOAD=${4:-$(mktemp)}
 if [ -z "$4" ]; then
   python2 energy_reading_forwarder.py payload > "$PAYLOAD"
