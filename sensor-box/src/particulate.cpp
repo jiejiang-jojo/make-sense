4c760ed29225894475653cfe8b913d042d0e16e4
Jie Jiang <jie.jiang@surrey.ac.uk>
2017-04-13 13:07:40 +0100

    Improve particulate sampling by averaging of multiple reads

--- sensor-box/src/particulate.cpp
+++ sensor-box/src/particulate.cpp
@@ -1,5 +1,7 @@
 #include "mbed.h"
+#include "rtos.h"
 #include "sensors.h"
+#include "config.h"
 
 //particulate sensor
 DigitalOut ledPower(P0_24);
@@ -8,22 +10,18 @@ AnalogIn analog_value(P0_25);
 int samplingTime = 280;
 int deltaTime = 40;
 int sleepTime = 9680;
-float dustDensityCN = 0, dustDensitySharp = 0, voMeasured = 0, voCalc = 0;
 
 float read_particulate()
 {
+  float dustDensityCN = 0, dustDensitySharp = 0, voMeasured = 0, voCalc = 0;
+  for(int i=0; i<PARTICULATE_SAMPLE_SIZE; i++){
     ledPower = 0; //power on the LED. Pull-down to activate
     wait_us(samplingTime);
     voMeasured = analog_value.read();
-    wait_us(deltaTime);
+    Thread::wait(deltaTime/1000.0);
     ledPower = 1; //turn the LED off. Pull up to turn off
-    wait_us(sleepTime);
-    voCalc = voMeasured*3.3;
-    dustDensitySharp = 0.5/2.8*(float(voCalc)-0.7);
-    dustDensityCN = (float(voCalc) - 0.0356)*1.2;
-
-    if(dustDensitySharp<0)
-        return 0;
-    else
-        return dustDensitySharp;
+    Thread::wait(sleepTime/1000.0);
+    voCalc += voMeasured*3.3;
+  }
+  return voCalc / PARTICULATE_SAMPLE_SIZE;
 }
